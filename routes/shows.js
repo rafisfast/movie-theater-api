af928f792d732b6c98a4d93b24a2176414b57e93
Rafay Tahir <rafayatahir@fb.com>
2022-11-11 12:16:30 +0000

    finished all routes and tested with postman

--- routes/shows.js
+++ routes/shows.js
@@ -1,27 +1,72 @@
 const express = require("express");
 const router = express.Router();
+const {User, User_show, Show} = require("../models/index");
 
-router.get("/shows", (req,res) => {
+router.get("/shows", async (req,res) => {
+    const shows = await Show.findAll();
+    if (!shows) return res.status(400).end();
+    res.status(200).send(shows)
+})
+
+router.get("/show/:id", async (req,res) => {
+    const id    = req.params.id;
+    if (!id) return res.status(400).end();
+    const show = await Show.findByPk(id);
+    if (!show) return res.status(400).end();
+    res.status(200).send(show)
+})
+
+router.get("/shows/:genre", async (req,res) => {
+    const genre  = req.params.genre;
+    if (!genre) return res.status(400).end();
+    const show = await Show.findAll({where: {genre: genre}});
+    if (!show) return res.status(400).end();
+    res.status(200).send(show)
+})
+
+router.put("/show/:id/rating/:rating", async (req,res) => {
+
+    const id  = req.params.id;
+    const rating  = req.params.rating;
+    if (!id || !rating) return res.status(400).end();
+
+    const show = await Show.findOne({where: {id: id}});
+
+    if (!show) return res.status(400).end();
+
+    const result = await show.update({rating : rating});
+
+    res.status(200).send(result)
 
 })
 
-router.get("/show/:id",(req,res) => {
+router.put("/show/:id/update", async (req,res) => {
+
+    const id  = req.params.id;
+    if (!id ) return res.status(400).end();
+
+    const show = await Show.findOne({where: {id: id}});
+
+    if (!show) return res.status(400).end();
+
+    const result = await show.update({status : show.status === "cancelled" ? "on-going" : "cancelled"});
+
+    res.status(200).send(result)
 
 })
 
-router.get("/show/:genre", (req,res) => {
+router.delete("/show/:id", async (req,res) => {
 
-})
+    const id  = req.params.id;
+    if (!id) return res.status(400).end();
 
-router.put("/show/:id/rating/:rating", (req,res) => {
+    const show = await Show.findByPk(id);
 
-})
+    if (!show) return res.status(400).end()
 
-router.put("/show/:id/status/:status", (req,res) => {
+    const result = await show.destroy();
 
-})
-
-router.delete("/show/:id", (req,res) => {
+    res.status(200).send(result)
 
 })
 
